eac5b608eb22addbc5f2f83fa1d78aadcc1d5acf
Mihail Struchalin <m.struchalin@gmail.com>
2022-08-30 00:29:50 +0300

    Change prereq.sh file

--- prereq.sh
+++ prereq.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-yum install "kernel-devel-$(uname -r)" -y
-
-yum install -y gcc perl kernel-headers kernel-devel libX11 libXt libXext libXmu elfutils-libelf-devel wget tree
+yum install -y "kernel-devel-$(uname -r)" gcc perl kernel-headers kernel-devel libX11 libXt libXext libXmu elfutils-libelf-devel wget tree
 
 reboot
